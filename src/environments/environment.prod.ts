ddd90a8291374575425fa720d6c05baa327b835b
intchl <126045100+intchl@users.noreply.github.com>
2024-06-01 12:29:40 +0700

    ionic-akademik done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCymGyqr-S-t_fPzsZKiS337dY2-XuBQDs',
+    authDomain: 'akademik2-c8566.firebaseapp.com',
+    projectId: 'akademik2-c8566',
+    storageBucket: 'akademik2-c8566.appspot.com',
+    messagingSenderId: '1058416772582',
+    appId: '1:1058416772582:web:e02a112ab07689b065c227',
+    measurementId: 'G-T5C9B85CSF',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,19 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyBN5W2h7xTmKTIgaIsiHQ0Hc6QIbB1xTug',
-    authDomain: 'akademik-7026d.firebaseapp.com',
-    projectId: 'akademik-7026d',
-    storageBucket: 'akademik-7026d.appspot.com',
-    messagingSenderId: '585143493203',
-    appId: '1:585143493203:web:13f54ea64c047623261ca6',
+    apiKey: 'AIzaSyCymGyqr-S-t_fPzsZKiS337dY2-XuBQDs',
+    authDomain: 'akademik2-c8566.firebaseapp.com',
+    projectId: 'akademik2-c8566',
+    storageBucket: 'akademik2-c8566.appspot.com',
+    messagingSenderId: '1058416772582',
+    appId: '1:1058416772582:web:e02a112ab07689b065c227',
+    measurementId: 'G-T5C9B85CSF',
+    // apiKey: 'AIzaSyBN5W2h7xTmKTIgaIsiHQ0Hc6QIbB1xTug',
+    // authDomain: 'akademik-7026d.firebaseapp.com',
+    // projectId: 'akademik-7026d',
+    // storageBucket: 'akademik-7026d.appspot.com',
+    // messagingSenderId: '585143493203',
+    // appId: '1:585143493203:web:13f54ea64c047623261ca6',
   },
 };
 
